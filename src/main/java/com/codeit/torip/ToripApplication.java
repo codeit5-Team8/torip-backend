1c6969f090b0521e91fdd2c61f3706c927b3579b
rappire <rappire@gmail.com>
2024-11-27 16:41:44 +0900

    feat: init project

---
+++ src/main/java/com/codeit/torip/ToripApplication.java
@@ -0,0 +1,13 @@
+package com.codeit.torip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToripApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToripApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codeit/torip/ToripApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeit.torip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToripApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
