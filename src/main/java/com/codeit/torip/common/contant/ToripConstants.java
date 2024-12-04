20042b176c5e01bb92bc18ead34db1b0d643c639
junghunee <cjswovkdnj12@naver.com>
2024-12-04 14:30:36 +0900

    * 글로벌 익셉션 핸들러 코드 수정

--- src/main/java/com/codeit/torip/common/contant/ToripConstants.java
+++ src/main/java/com/codeit/torip/common/contant/ToripConstants.java
@@ -14,6 +14,8 @@ public class ToripConstants {
         public static final Integer SUCCESS_CODE = 200;
         public static final Integer SERVER_FAIL_CODE = 500;
         public static final Integer CLIENT_FAIL_CODE = 400;
+        public static final Integer FORBIDDEN_CODE = 403;
+        public static final Integer METHOD_NOT_ALLOWED_CODE = 405;
 
         public static final String SUCCESS_MESSAGE = "성공";
         public static final String FAIL_MESSAGE = "실패";
--- src/main/java/com/codeit/torip/common/exception/GlobalExceptionHandler.java
+++ src/main/java/com/codeit/torip/common/exception/GlobalExceptionHandler.java
@@ -5,11 +5,14 @@ import io.jsonwebtoken.ExpiredJwtException;
 import jakarta.validation.ConstraintViolationException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.dao.DataAccessException;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import static com.codeit.torip.common.contant.ToripConstants.HttpConstant.*;
 
 @Slf4j
 @RestControllerAdvice
@@ -17,49 +20,49 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(ExpiredJwtException.class)
     public ResponseDto expiredJwtExceptionHandler(ExpiredJwtException e) {
-        return ResponseDto.fail(403, e.getMessage());
+        return ResponseDto.fail(FORBIDDEN_CODE, e.getMessage());
     }
 
     @ExceptionHandler(Exception.class)
     public ResponseDto defaultExceptionHandler(Exception e) {
         log.error("Exception", e);
-        return ResponseDto.fail(500, e.getMessage());
+        return ResponseDto.fail(SERVER_FAIL_CODE, e.getMessage());
     }
 
     @ExceptionHandler(DataAccessException.class)
     public ResponseDto dataAccessExceptionHandler(DataAccessException e) {
         log.error("DataAccessException", e);
-        return ResponseDto.fail(400, e.getMessage());
+        return ResponseDto.fail(CLIENT_FAIL_CODE, e.getMessage());
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseDto methodArgumentNotValidExceptionHandler(MethodArgumentNotValidException e) {
         log.error("MethodArgumentNotValidException", e);
-        return ResponseDto.fail(400, e.getMessage());
+        return ResponseDto.fail(CLIENT_FAIL_CODE, e.getMessage());
     }
 
     @ExceptionHandler(ConstraintViolationException.class)
     public ResponseDto constraintViolationExceptionHandler(ConstraintViolationException e) {
         log.error("ConstraintViolationException", e);
-        return ResponseDto.fail(400, e.getMessage());
+        return ResponseDto.fail(CLIENT_FAIL_CODE, e.getMessage());
     }
 
     @ExceptionHandler(HttpMessageNotReadableException.class)
     public ResponseDto httpMessageNotReadableExceptionHandler(HttpMessageNotReadableException e) {
         log.error("HttpMessageNotReadableException", e);
-        return ResponseDto.fail(400, e.getMessage());
+        return ResponseDto.fail(CLIENT_FAIL_CODE, e.getMessage());
     }
 
     @ExceptionHandler(IllegalArgumentException.class)
     public ResponseDto illegalArgumentExceptionHandler(IllegalArgumentException e) {
-        log.error("handelIllegalArgumentException", e);
-        return ResponseDto.fail(400, e.getMessage());
+        log.error("lIllegalArgumentException", e);
+        return ResponseDto.fail(CLIENT_FAIL_CODE, e.getMessage());
     }
 
     @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
     public ResponseDto httpRequestMethodNotSupportedExceptionHandler(HttpRequestMethodNotSupportedException e) {
         log.error("HttpRequestMethodNotSupportedException", e);
-        return ResponseDto.fail(405, e.getMessage());
+        return ResponseDto.fail(METHOD_NOT_ALLOWED_CODE, e.getMessage());
     }
 
 }
