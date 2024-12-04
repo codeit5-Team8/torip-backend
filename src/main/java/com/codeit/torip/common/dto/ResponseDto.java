2c50e8e8c7ebf4b12bd30269bc747e712a7d53a3
mz01-junghunee <cjswovkdnj12@naver.com>
2024-12-04 11:30:45 +0900

    * 익셉션 핸들러 추가

--- src/main/java/com/codeit/torip/common/dto/ResponseDto.java
+++ src/main/java/com/codeit/torip/common/dto/ResponseDto.java
@@ -1,5 +1,6 @@
 package com.codeit.torip.common.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -14,9 +15,13 @@ import static com.codeit.torip.common.contant.ToripConstants.HttpConstant.SUCCES
 @AllArgsConstructor
 public class ResponseDto {
 
+    @Schema(description = "성공 여부")
     private Boolean success;
+    @Schema(description = "결과 코드")
     private Integer code;
+    @Schema(description = "결과 메세지")
     private String message;
+    @Schema(description = "결과 데이터")
     private Object result;
 
     public static ResponseDto success(Object result) {
@@ -25,6 +30,14 @@ public class ResponseDto {
                 .code(SUCCESS_CODE)
                 .message(SUCCESS_MESSAGE)
                 .result(result)
+                .build();
+    }
+
+    public static ResponseDto fail(Integer code, String message) {
+        return ResponseDto.builder()
+                .success(false)
+                .code(code)
+                .message(message)
                 .build();
     }
 
