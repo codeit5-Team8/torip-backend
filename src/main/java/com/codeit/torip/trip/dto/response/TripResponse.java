34456bc34fbac9545c00d688adddb1bb3a0f2a07
rappire <rappire@gmail.com>
2024-12-12 20:55:32 +0900

    feat: 리턴 타입에 ID 추가 및 설명 추가

--- src/main/java/com/codeit/torip/trip/dto/response/TripResponse.java
+++ src/main/java/com/codeit/torip/trip/dto/response/TripResponse.java
@@ -1,6 +1,7 @@
 package com.codeit.torip.trip.dto.response;
 
 import com.codeit.torip.user.dto.UserResponse;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -12,12 +13,32 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 @Getter
 public class TripResponse {
+    @Schema(description = "여행의 id", example = "1")
     private Long id;
+    @Schema(description = "여행의 이름", example = "제주도 여행")
     private String name;
+    @Schema(description = "여행의 시작 날짜", example = "2021-10-01")
     private LocalDate startDate;
+    @Schema(description = "여행의 종료 날짜", example = "2021-10-05")
     private LocalDate endDate;
+    @Schema(description = "여행을 만든 사람", example = """
+            {
+            "id": 1,
+            "username": "test",
+            "email": "test@test.com"
+            }
+            """)
     private UserResponse owner;
+    @Schema(description = "여행을 생성한 시간", example = "2021-10-01T00:00:00")
     private LocalDateTime createdAt;
+    @Schema(description = "여행을 마지막으로 변경한 사람", example = """
+            {
+                "id": 1,
+                "username": "test",
+                "email": "test@test.com"
+            }
+            """)
     private UserResponse lastUpdatedUser;
+    @Schema(description = "여행을 마지막으로 업데이트한 시간", example = "2021-10-01T00:00:00")
     private LocalDateTime updatedAt;
 }
